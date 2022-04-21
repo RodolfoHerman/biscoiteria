e1d76ff10cfe661dd4867fb1b9d7b094df92042b
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2022-04-21 11:11:34 -0300

    commit inicial

---
+++ src/main/java/br/com/rodolfo/biscoiteria/BiscoiteriaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rodolfo.biscoiteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiscoiteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiscoiteriaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/rodolfo/biscoiteria/BiscoiteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rodolfo.biscoiteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiscoiteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
