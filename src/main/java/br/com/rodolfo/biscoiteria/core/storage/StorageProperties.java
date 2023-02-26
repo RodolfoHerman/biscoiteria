a3d73254f59efa40b53a22f0600b97de19559c32
Rodolfo Herman <ciencia.rodolfo@gmail.com>
2023-02-26 20:23:47 -0300

    Criando a classe StorageProperties para recuperar informações do application.properties

---
+++ src/main/java/br/com/rodolfo/biscoiteria/core/storage/StorageProperties.java
@@ -0,0 +1,37 @@
+package br.com.rodolfo.biscoiteria.core.storage;
+
+import java.nio.file.Path;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Component
+@ConfigurationProperties("biscoiteria.storage")
+public class StorageProperties {
+
+    private Local local = new Local();
+    private S3 s3 = new S3();
+
+    @Getter
+    @Setter
+    public class Local {
+
+        private Path diretorioFotos;
+    }
+
+    @Getter
+    @Setter
+    public class S3 {
+
+        private String idChaveAcesso;
+        private String chaveAcessoSecreta;
+        private String regiao;
+        private String bucket;
+        private String diretorioFotos;
+    }
+}
